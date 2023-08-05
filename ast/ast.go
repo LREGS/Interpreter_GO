97b426f395484df76e7b52004aff68a100144f8a
silliwilli <william-cullum@hotmail.co.uk>
2023-08-05 08:03:34 +0100

    idfentifier, expression and let types/functions

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast 
 
+import {
+	"/home/silliwilli/Desktop/interpreter/Interpreter_GO/token"
+}
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -13,3 +17,32 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct{
+	Token token.Token 
+	name *Identifier
+	Value Expression
+}
+
+func (p *Program) TokenLiteral() string{
+	if len(p.Statements) > 0 {
+		return p.Statement[0].TokenLiteral()
+	}else{
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode()		{}
+func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func(i *Identifier) expressionNode()		{}
+func(i *Identifier) TokenLiteral() string{ return i.Token.Literal}
